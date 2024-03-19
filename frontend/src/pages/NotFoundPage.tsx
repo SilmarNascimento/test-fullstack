ee0a3cbe8d7368e59a117ae5cfe954c1e929ae9e
SilmarNascimento <silmarfnascimento@gmail.com>
2024-03-18 22:14:37 -0300

    fix: altera componennte de NotFoundPage e retira pasta de testes

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,10 +1,19 @@
 import { Link } from "react-router-dom"
+import { Button } from "../components/ui/button"
+import { SearchX } from "lucide-react"
 
 export function NotFoundPage() {
   return (
-    <>
-      <h1>Página não encontrada</h1>
-      <Link to={"/"}>Lista de Usuários</Link>
-    </>
+    <div className="flex flex-col gap-6">
+      <h1 className="h-40 w flex flex-col items-center justify-center">
+        <SearchX className="size-32"/>
+        Página não encontrada
+      </h1>
+      <Link to={"/"}>
+        <Button>
+          Lista de Usuários
+        </Button>
+      </Link>
+    </div>
   )
 }
