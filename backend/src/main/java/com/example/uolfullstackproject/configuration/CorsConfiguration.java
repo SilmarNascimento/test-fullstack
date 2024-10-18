6e48b15afca7fac43ef1a746b72a4c2a3930a852
SilmarNascimento <silmarfnascimento@gmail.com>
2024-10-18 12:45:06 -0300

    refactor: adiciona mais features ao readme, adiciona varaiveis de ambiente para configurar o CORS da aplicação dinamicamente

--- backend/src/main/java/com/example/uolfullstackproject/configuration/CorsConfiguration.java
+++ backend/src/main/java/com/example/uolfullstackproject/configuration/CorsConfiguration.java
@@ -1,16 +1,24 @@
 package com.example.uolfullstackproject.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
+  @Value("${spring.frontend.domain}")
+  private String frontendDomain;
+
+  @Value("${spring.frontend.port}")
+  private String frontendPort;
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
+    String frontendUrl = "http://" + frontendDomain + ":" + frontendPort;
+
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000")
+        .allowedOrigins(frontendUrl)
         .allowedMethods("GET", "POST", "PUT", "DELETE");
   }
 
